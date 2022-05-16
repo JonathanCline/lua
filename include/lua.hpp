c933724c98de888020e2ee4b0d656c66da5ada78
JonathanCline <jonathancline64@gmail.com>
2022-05-16 15:53:39 -0500

    Moved header files to allow more natural include path "lua/*.h".
    The previously functioning "*.h" still works.

--- include/lua.hpp
+++ include/lua.hpp
@@ -1,20 +1,2 @@
-#pragma once
-#ifndef LUA_HPP
-#define LUA_HPP
-
-// Adds the extern "C" directive around C source headers
-
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-	#include "lua.h"
-	#include "lualib.h"
-	#include "lauxlib.h"
-
-#ifdef __cplusplus
-};
-#endif
-
-#endif
+// Exposes a lua header file that has been moved in order to keep everything building.
+#include "lua/lua.hpp"
